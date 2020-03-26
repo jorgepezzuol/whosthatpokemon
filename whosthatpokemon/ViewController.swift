e602700d77b6ce720799324d7941d3097d485a8d
Jorge Pezzuol <jorge@generous.studio>
2020-03-26 03:24:16 +0000

    first version of the layout

---
+++ whosthatpokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whosthatpokemon
+//
+//  Created by Generous on 25/03/2020.
+//  Copyright © 2020 Pezzuol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
